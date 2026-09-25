2699b04af2efba6e1ea30caff36396753598833e
arhennia <chinarireddy2005@gmail.com>
2026-09-25 09:37:06 +0530

    Create longest_common_subsequence.c

---
+++ day-8/longest_common_subsequence.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LEN 1024
+
+// Longest Common Subsequence using Dynamic Programming
+void findLCS(const char *X, const char *Y) {
+    int m = (int)strlen(X);
+    int n = (int)strlen(Y);
+
+    // Allocate DP table (m+1) x (n+1)
+    int **L = (int **)malloc((m + 1) * sizeof(int *));
+    for (int i = 0; i <= m; i++) {
+        L[i] = (int *)calloc(n + 1, sizeof(int));
+    }
+
+    // Build the LCS table in bottom-up manner
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (X[i - 1] == Y[j - 1]) {
+                L[i][j] = L[i - 1][j - 1] + 1;
+            } else {
+                L[i][j] = (L[i - 1][j] >= L[i][j - 1]) ? L[i - 1][j] : L[i][j - 1];
+            }
+        }
+    }
+
+    int lcsLength = L[m][n];
+
+    // Reconstruct LCS string from DP table
+    char *lcsStr = (char *)malloc((lcsLength + 1) * sizeof(char));
+    lcsStr[lcsLength] = '\0';
+
+    int i = m, j = n;
+    int index = lcsLength - 1;
+
+    while (i > 0 && j > 0) {
+        if (X[i - 1] == Y[j - 1]) {
+            lcsStr[index--] = X[i - 1];
+            i--;
+            j--;
+        } else if (L[i - 1][j] >= L[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    // Display results
+    printf("\nOutput:\n");
+    printf("LCS: %s\n", lcsStr);
+    printf("LCS Length: %d\n", lcsLength);
+
+    // Free memory
+    free(lcsStr);
+    for (int k = 0; k <= m; k++) {
+        free(L[k]);
+    }
+    free(L);
+}
+
+int main(void) {
+    char str1[MAX_LEN];
+    char str2[MAX_LEN];
+
+    printf("Enter the first string into an array: ");
+    if (scanf("%1023s", str1) != 1) {
+        printf("Invalid first string.\n");
+        return 1;
+    }
+
+    printf("Enter the second string into an array: ");
+    if (scanf("%1023s", str2) != 1) {
+        printf("Invalid second string.\n");
+        return 1;
+    }
+
+    findLCS(str1, str2);
+
+    return 0;
+}
